9c557c86ce366765930446b69df0c7a07d4ee44f
Gustavo <prestesgustavo999@gmail.com>
2023-08-17 22:25:33 -0400

    modified:   lib/login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'main.dart';
 
 class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
